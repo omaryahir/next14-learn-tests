790a4574a90542bb442bb713aded1e04234461a1
Omar Mendoza <1206997+omaryahir@users.noreply.github.com>
2024-04-01 20:12:45 -0600

    20240401 Enabling data info for the revenue

---
+++ app/dashboard/loading.tsx
@@ -0,0 +1,4 @@
+export default function Loading() {
+  // return <b><div className="grid-cols-2"><img src="https://i.stack.imgur.com/kOnzy.gif" width={50} /></div><div>Loading...</div></b>
+  return <b><img src="https://i.stack.imgur.com/kOnzy.gif" className="inline mr-5" width={30} /> Loading...</b> 
+}
