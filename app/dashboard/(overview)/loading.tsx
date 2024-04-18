c8edf8413b88516753797fc8e3d2c9828efe08ec
Omar Mendoza <1206997+omaryahir@users.noreply.github.com>
2024-04-18 06:31:31 -0600

    feat: c 14 add lint for accessibility and fix loading image

--- app/dashboard/(overview)/loading.tsx
+++ app/dashboard/(overview)/loading.tsx
@@ -1,4 +1,6 @@
+import Image from 'next/image';
+
 export default function Loading() {
-  // return <b><div className="grid-cols-2"><img src="https://i.stack.imgur.com/kOnzy.gif" width={50} /></div><div>Loading...</div></b>
-  return <b><img src="https://i.stack.imgur.com/kOnzy.gif" className="inline mr-5" width={30} /> Loading...</b> 
+  return <b><Image src="https://i.stack.imgur.com/kOnzy.gif"
+  className="inline mr-5" width={30} height={30} alt="loading" /> Loading...</b> 
 }
