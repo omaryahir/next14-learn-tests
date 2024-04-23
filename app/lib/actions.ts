b9d78c9286594420d91c6342f679e4c0f4b31b05
Omar Mendoza <1206997+omaryahir@users.noreply.github.com>
2024-04-23 11:49:24 -0600

    feat: accessibility part 2 - form validation

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -39,6 +39,8 @@ export async function createInvoice(prevState: State, formData: FormData) {
     amount: formData.get('amount'),
     status: formData.get('status'),
   });
+  // Temporary console.log to see the shape
+  console.log(validatedFields);
 
   // If form validation fails, return errors early. Otherwise, continue.
   if (!validatedFields.success) {
@@ -48,20 +50,25 @@ export async function createInvoice(prevState: State, formData: FormData) {
     }
   }
 
+  // Prepare data for insertion into the database
+  const { customerId, amount, status } = validatedFields.data;
   const amountInCents = amount * 100;
   const date = new Date().toISOString().split('T')[0];
 
+  // Insert data into the database
   try {
     await sql`
       INSERT INTO invoices (customer_id, amount, status, date)
       VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
     `;
   } catch (error) {
+    // If a database error ocurrs, return a more specific error.
     return {
       message: `Database Error: Failed to Create Invoice`
     }
   }
 
+  // Revalidate the cache for the invoices page and redirect the user.
   revalidatePath('/dashboard/invoices');
   redirect('/dashboard/invoices');
 }
